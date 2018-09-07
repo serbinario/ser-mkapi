103a9975433e6d6ceb95320bb12f505f2c749117
Pancosta <dz6.gustav@gmail.com>
2018-09-07 14:15:56 -0300

    first commit

--- app/Console/Commands/LogCron.php
+++ app/Console/Commands/LogCron.php
@@ -15,7 +15,7 @@ class LogCron extends Command
      *
      * @var string
      */
-    protected $signature = 'log:cron';
+    protected $signature = 'log:cron {var : The ID of the user}';
 
     /**
      * The console command description.
@@ -42,12 +42,31 @@ class LogCron extends Command
     public function handle()
     {
 
+        $var = $this->argument('var');
+
+        switch($var){
+            case "updateInadiplentes":
+                $this->updateInadiplentes();
+                break;
+            case "listclients":
+                $this->listClients();
+                break;
+            case "ssh";
+                $this->ssh2();
+                //$this->cfgssh('170.245.65.134', 'NetSerb', 'nets@2017#', 'ppp active print  without-paging', '22', 'TUDO');
+                break;
+            default:
+                echo "not commands \n";
+
+
+        }
+
         //RN-0004
         //Pega o dia atual + 10 dias
         // msg = 1 vencendo
         // msg = 2 Vencido
         // msg = 3 bloqueio
-        $dateNow = Carbon::now()->addDays(10)->format('Y-m-d');
+        /*$dateNow = Carbon::now()->addDays(10)->format('Y-m-d');
         $debitos = Debitos::with('mkCliente')->where('data_vencimento','<=', $dateNow)
             ->whereIn('status_id', [4,2])
             ->orderBy('data_vencimento', 'asc')
@@ -77,7 +96,18 @@ class LogCron extends Command
                 $this->aVencer($debito, $length);
                 //echo   $debito->mkCliente->nome ." - " . $debito->id . " - ". $debito->status_id . " - ". $debito->data_vencimento . " - " . $length . "\n" ;
             }
-        }
+        }*/
+    }
+
+    /*
+     * Atualiza a tabela fin_debitos se algum debito na data atual + 2 dias, nao estiver como pago, e colocado como inadimplente
+     */
+    public function updateInadiplentes()
+    {
+        $rows = \DB::table('fin_debitos')
+            ->where('status_id', 2)
+            ->whereRaw('data_vencimento = CURRENT_DATE()-2')
+            ->update(['status_id' => 4]);
     }
 
     public function vencidos($debito, $length)
